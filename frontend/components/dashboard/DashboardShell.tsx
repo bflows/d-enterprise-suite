4b9c78dddd7fbb7db6400a37185b7c0cab8a7e01
flowz0 <billylflowers@gmail.com>
2026-02-24 20:55:50 -0800

    feat(dashboard): refine layout structure in DashboardShell and DashboardPage components

--- frontend/components/dashboard/DashboardShell.tsx
+++ frontend/components/dashboard/DashboardShell.tsx
@@ -24,25 +24,19 @@ export default function DashboardShell({
 
   return (
     <RequireAuth>
-      {/* <div className="flex bg-neutral-200">
-        <DashboardSidebar />
-        <main className="min-h-screen w-full px-16 py-8">
-          {children}
-        </main>
-      </div> */}
       <div className="flex flex-col sm:flex-row min-h-screen">
         {/* Navbar (mobile) */}
         <div className="top-0 sticky sm:hidden">
           Navbar
         </div>
         {/* Sidebar (desktop) */}
-        <div className="hidden sm:block sm:w-48 md:w-80 border-2 border-green-400">
+        <div className="hidden sm:block sm:w-48 md:w-64">
           Sidebar
         </div>
         {/* Display (both) */}
-        <div className="flex-1 border-2 border-red-400">
+        <main className="flex-1">
           {children}
-        </div>
+        </main>
         {/* BottomNav (mobile) */}
         <div className="bottom-0 sticky sm:hidden">
           Bottom Nav
