e9838f61893c7729d1624f82fba1ec5b0e3a3ecc
flowz0 <billylflowers@gmail.com>
2026-02-24 23:05:15 -0800

    fix(employees): adjust column widths in EmployeesTable for better layout

--- frontend/components/employees/EmployeesTable.tsx
+++ frontend/components/employees/EmployeesTable.tsx
@@ -73,7 +73,7 @@ export default function EmployeesTable() {
               <input
                 type="text"
                 placeholder="Name or phone"
-                className="bg-neutral-100 text-neutral-600 text-p border border-neutral-400 rounded-lg py-3 pl-12 pr-4 focus:outline-none focus:border focus:ring focus:ring-primary focus:border-primary placeholder:text-neutral-400"
+                className="bg-neutral-100 text-neutral-600 text-p w-48 border border-neutral-400 rounded-lg py-3 pl-12 pr-4 focus:outline-none focus:border focus:ring focus:ring-primary focus:border-primary placeholder:text-neutral-400"
               />
             </div>
             <Link
@@ -90,16 +90,16 @@ export default function EmployeesTable() {
                 <th className="w-60 text-left text-neutral-600 text-p font-normal">
                   Name
                 </th>
-                <th className="w-60 text-left text-neutral-600 text-p font-normal">
+                <th className="w-72 text-left text-neutral-600 text-p font-normal">
                   Phone
                 </th>
-                <th className="w-60 text-left text-neutral-600 text-p font-normal">
+                <th className="w-60 text-left text-neutral-600 text-p font-normal hidden lg:table-cell">
                   Email
                 </th>
-                <th className="w-60 text-left text-neutral-600 text-p font-normal">
+                <th className="w-48 lg:w-60 text-left text-neutral-600 text-p font-normal">
                   Position
                 </th>
-                <th className="w-60 text-left text-neutral-600 text-p font-normal">
+                <th className="w-24 lg:w-60 text-left text-neutral-600 text-p font-normal">
                   Actions
                 </th>
               </tr>
@@ -125,7 +125,7 @@ export default function EmployeesTable() {
                     <td className="text-neutral-800 text-p text-left pt-4">
                       {emp.user.phoneNumber || "—"}
                     </td>
-                    <td className="text-neutral-800 text-p text-left pt-4">
+                    <td className="text-neutral-800 text-p text-left pt-4 hidden lg:table-cell">
                       {emp.user.email || "—"}
                     </td>
                     <td className="text-neutral-800 text-p text-left pt-4 capitalize">
