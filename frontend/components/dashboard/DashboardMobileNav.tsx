eead96e159a5a0564ccaff2bad21c96f6c6e0378
flowz0 <billylflowers@gmail.com>
2026-02-24 23:05:02 -0800

    fix(dashboard): adjust padding in DashboardMobileNav for better alignment

--- frontend/components/dashboard/DashboardMobileNav.tsx
+++ frontend/components/dashboard/DashboardMobileNav.tsx
@@ -6,22 +6,22 @@ export default function DashboardMobileNav() {
     <div className="bg-neutral-50 h-20 px-6 bottom-0 sticky border-t border-neutral-400 sm:hidden">
       <div className="flex items-center justify-evenly h-full">
         <Link href='/dashboard' className="h-full">
-          <div className="h-full flex items-center px-6">
+          <div className="h-full flex items-center px-4">
             <LuHouse className="text-neutral-600 size-8" />
           </div>
         </Link>
         <Link href='/schedule' className="h-full">
-          <div className="h-full flex items-center px-6">
+          <div className="h-full flex items-center px-4">
             <LuCalendarDays className="text-neutral-600 size-8" />
           </div>
         </Link>
         <Link href='/customers' className="h-full">
-          <div className="h-full flex items-center px-6">
+          <div className="h-full flex items-center px-4">
             <LuBookUser className="text-neutral-600 size-8" />
           </div>
         </Link>
         <Link href='/inbox' className="h-full">
-          <div className="h-full flex items-center px-6">
+          <div className="h-full flex items-center px-4">
             <LuInbox className="text-neutral-600 size-8" />
           </div>
         </Link>
