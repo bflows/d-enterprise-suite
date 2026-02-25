fd8440eba7d2ed173c71ebbd8942ec3aa7413520
flowz0 <billylflowers@gmail.com>
2026-02-24 17:14:28 -0800

    feat(dashboard): adjust layout spacing in DashboardShell and DashboardSidebar components

--- frontend/components/dashboard/DashboardSidebar.tsx
+++ frontend/components/dashboard/DashboardSidebar.tsx
@@ -3,8 +3,8 @@ import { LuHouse, LuUsers } from "react-icons/lu";
 
 export default function DashboardSidebar() {
   return (
-    <nav className="w-80 py-8 px-6 bg-neutral-50 border-r border-neutral-400">
-      <div className="">
+    <nav className="w-96 py-8 px-6 bg-neutral-50 border-r border-neutral-400">
+      <div>
         <div className="w-12 h-12 bg-neutral-200"></div>
         <h1 className="text-neutral-900 text-h5 font-bold mt-4">
           Duct Daddy
@@ -12,11 +12,11 @@ export default function DashboardSidebar() {
       </div>
 
       {/* Company */}
-      <div className="mt-16">
+      <div className="mt-12">
         <h2 className="text-neutral-400 text-small uppercase font-bold">
           Home
         </h2>
-        <ul className="mt-2">
+        <ul className="mt-1">
           <li>
             <Link href='/dashboard' className="flex items-center gap-x-2 px-4 py-2 rounded-lg group transition-colors duration-300 ease-in-out hover:bg-primary/10">
               <LuHouse className="text-neutral-600 size-6 transition-colors duration-300 ease-in-out group-hover:text-primary" />
@@ -33,7 +33,7 @@ export default function DashboardSidebar() {
         <h2 className="text-neutral-400 text-small uppercase font-bold">
           Company
         </h2>
-        <ul className="mt-2">
+        <ul className="mt-1">
           <li>
             <Link href='/employees' className="flex items-center gap-x-2 px-4 py-2 rounded-lg group transition-colors duration-300 ease-in-out hover:bg-primary/10">
               <LuUsers className="text-neutral-600 size-6 transition-colors duration-300 ease-in-out group-hover:text-primary" />
