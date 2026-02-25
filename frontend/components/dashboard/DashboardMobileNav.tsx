54b78c80f1da03d29123d4edb79b7ff0fef14510
flowz0 <billylflowers@gmail.com>
2026-02-24 22:46:49 -0800

    feat(dashboard): add DashboardMobileNav component for mobile navigation

---
+++ frontend/components/dashboard/DashboardMobileNav.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { LuBookUser, LuCalendarDays, LuHouse, LuInbox } from "react-icons/lu";
+
+export default function DashboardMobileNav() {
+  return (
+    <div className="bg-neutral-50 h-20 px-6 bottom-0 sticky border-t border-neutral-400 sm:hidden">
+      <div className="flex items-center justify-evenly h-full">
+        <Link href='/dashboard' className="h-full">
+          <div className="h-full flex items-center px-6">
+            <LuHouse className="text-neutral-600 size-8" />
+          </div>
+        </Link>
+        <Link href='/schedule' className="h-full">
+          <div className="h-full flex items-center px-6">
+            <LuCalendarDays className="text-neutral-600 size-8" />
+          </div>
+        </Link>
+        <Link href='/customers' className="h-full">
+          <div className="h-full flex items-center px-6">
+            <LuBookUser className="text-neutral-600 size-8" />
+          </div>
+        </Link>
+        <Link href='/inbox' className="h-full">
+          <div className="h-full flex items-center px-6">
+            <LuInbox className="text-neutral-600 size-8" />
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
