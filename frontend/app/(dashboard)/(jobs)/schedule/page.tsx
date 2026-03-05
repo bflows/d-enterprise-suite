657a4d4486e04628b1c4aeb5be3e4cbf35e18695
flowz0 <billylflowers@gmail.com>
2026-03-05 10:14:36 -0800

    feat(schedule): add 'New Job' button with icon and improved layout for enhanced user experience

--- frontend/app/(dashboard)/(jobs)/schedule/page.tsx
+++ frontend/app/(dashboard)/(jobs)/schedule/page.tsx
@@ -1,7 +1,21 @@
+import { LuPlus } from "react-icons/lu";
+
 export default function SchedulePage() {
   return (
     <div>
-      <h1 className="text-neutral-900 text-h4 font-bold">Schedule</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-neutral-900 text-h4 font-bold">Schedule</h1>
+        <button
+          className="bg-primary text-neutral-200 text-p font-bold py-3 px-4 rounded-lg flex items-center gap-x-2 cursor-pointer transition-colors hover:bg-primary/90 hover:text-neutral-50"
+        >
+          <div>
+            <LuPlus className="size-6" />
+          </div>
+          New Job
+        </button>
+      </div>
+
+
     </div>
   );
 }
